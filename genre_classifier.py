0edd5c756383cd408980b8af79de89ca2949ead8
muditsood7 <muditsood123@gmail.com>
2024-08-19 21:19:23 -0400

    added changes to reduce overfitting

--- genre_classifier.py
+++ genre_classifier.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import matplotlib.pyplot as plt
 
 DATASET_PATH = "Data_full.json"
 
@@ -15,6 +16,26 @@ def load_data(dataset_path):
 
         return inputs, targets
 
+def plot_history(history):
+
+    fig, axs = plt.subplots(2)
+
+    # create accuracy subplot
+    axs[0].plot(history.history["accuracy"], label="Training Accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="Test Accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy Evaluation")
+
+    # create accuracy subplot
+    axs[1].plot(history.history["loss"], label="Training Error")
+    axs[1].plot(history.history["val_loss"], label="Test Error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error Evaluation")
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -32,13 +53,16 @@ if __name__ == "__main__":
         keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
 
         # first hidden layer
-        keras.layers.Dense(512, activation='relu'),
+        keras.layers.Dense(512, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # second hidden layer
-        keras.layers.Dense(256, activation='relu'),
+        keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # third hidden layer
-        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # output layer
         keras.layers.Dense(10, activation='softmax')
@@ -51,7 +75,10 @@ if __name__ == "__main__":
     model.summary()
 
     # train network
-    model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test), epochs=50, batch_size=32)
+    history = model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test), epochs=50, batch_size=32)
+
+    # plot accuracy and error over the epochs
+    plot_history(history)
 
 
 
